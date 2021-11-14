6050a7918fae41229bec538c1911e74af18553ab
Samuel VanDenburgh <vandensp@rose-hulman.edu>
2021-11-14 05:24:16 +0000

    LEDs can update through flask (Tom Kirchhoffer did a major part of this)

---
+++ onlineLEDstrips/test.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+import time
+import math
+import Adafruit_BBIO.GPIO as GPIO
+import sys
+
+#GPIO Button Setup
+colorButton = "P8_15"
+flashButton = "P8_10"
+GPIO.setup(colorButton, GPIO.IN) 
+GPIO.setup(flashButton, GPIO.IN) 
+
+LEDlength = 60
+print(sys.argv)
+r=int(sys.argv[1])
+g=int(sys.argv[2])
+b=int(sys.argv[3])
+current = 0
+amp = 12
+f = 5
+shift = 3
+phase = 0 
+currFlash = 0
+flashSpeeds = [float(sys.argv[4]),0, 0.1, 0.25, 0.5, 1]
+flashLength = len(flashSpeeds)
+flashDelay = float(sys.argv[4])
+
+colors = [[int(sys.argv[1]),int(sys.argv[2]),int(sys.argv[3])],[1,0,0],[1,1,0],[0,1,0],[0,1,1],[0,0,1],[1,0,1], [1,1,1], [0,0,0]]
+length = len(colors)
+
+# Open a file
+fo = open("/dev/rpmsg_pru30", "wb", 0)
+
+
+def changeColor(colorButton):
+    global current
+    global length
+    current += 1 
+    current %= length
+    color = colors[current]
+    r = color[0]
+    g = color[1]
+    b = color[2]
+    time.sleep(0.05) #debounce delay
+    for i in range(0, LEDlength):
+        fo.write(b"%d %d %d %d\n" % (i, r, g, b))
+    fo.write(b"-1 0 0 0\n");    # Send colors to LEDs
+
+def changeFlash(flashButton):
+    global currFlash
+    global flashLength
+    global flashDelay
+    currFlash += 1
+    currFlash %= flashLength
+    flashDelay = flashSpeeds[currFlash]
+    print("FlashDelay: ", flashDelay)
+
+GPIO.add_event_detect(colorButton, GPIO.FALLING, callback=changeColor)
+GPIO.add_event_detect(flashButton, GPIO.FALLING, callback=changeFlash)
+
+for i in range(0, LEDlength):
+    fo.write(b"%d %d %d %d\n" % (i, r, g, b))
+fo.write(b"-1 0 0 0\n");    # Send colors to LEDs
+
+while True:
+    if current==length-1:
+        for i in range(0, LEDlength):
+            r = (amp * (math.sin(2*math.pi*f*(i-phase-0*shift)/LEDlength) + 1)) + 1;
+            g = (amp * (math.sin(2*math.pi*f*(i-phase-1*shift)/LEDlength) + 1)) + 1;
+            b = (amp * (math.sin(2*math.pi*f*(i-phase-2*shift)/LEDlength) + 1)) + 1;
+            fo.write(b"%d %d %d %d\n" % (i, r, g, b))
+        fo.write(b"-1 0 0 0\n");
+        phase=phase+1+(flashDelay*5)
+        time.sleep(0.05)
+    else:
+        color = colors[current]
+        r = color[0]
+        g = color[1]
+        b = color[2]
+        for i in range(0, LEDlength):
+            fo.write(b"%d %d %d %d\n" % (i, r, g, b))
+        fo.write(b"-1 0 0 0\n");
+    
+    if flashDelay != 0:
+        time.sleep(flashDelay)
+        for i in range(0, LEDlength):
+            fo.write(b"%d %d %d %d\n" % (i, 0, 0, 0))
+        fo.write(b"-1 0 0 0\n");
+        time.sleep(flashDelay)
+    
+# Close opened file
+fo.close()
