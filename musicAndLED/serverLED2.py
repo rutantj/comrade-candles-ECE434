e34da413731b7d798e8f6c5d209b419b76d5485d
Demo User <debian@beaglebone.localdomain>
2021-11-16 18:18:53 +0000

    Added changes from Monday night before polishing project

--- musicAndLED/serverLED2.py
+++ musicAndLED/serverLED2.py
@@ -5,7 +5,7 @@ import Adafruit_BBIO.GPIO as GPIO
 from flask import Flask, render_template, request
 import os
 
-audiofiles = ["DLF11.mp3", "'Leave The Broom Where It Is.mp3'", "'Take A Breath.mp3'", "DLF4.mp3" "'Put On Your Shades.mp3'", "'You Should Go.mp3'"]
+files = ["DLF11.mp3", "'Leave The Broom Where It Is.mp3'", "'Take A Breath.mp3'", "DLF4.mp3" "'Put On Your Shades.mp3'", "'You Should Go.mp3'"]
 
 #online stuff ##################################################################################################
 
@@ -17,22 +17,18 @@ def index():
         }
 	return render_template('remoteVirtual.html', **templateData)
 	
-@app.route("/<musicIndex>/<isSent>")
-def action(red, green, blue, flash):
-	#   Send same url with browserAction changed to "no" to other bone.
-	os.system("pkill -f mplayer")
-	if(isSent == "false"):
-		os.system("wget --spider --server-response vandensp.pagekite.me:8081/musicIndex/true")
-	os.system("mplayer -ao alsa:device=sysdefault=AT2020USB ../mp3folder/"+files[musicIndex])
-	templateData={
-	}
-	return render_template('remoteVirtual.html', **templateData)
-	
-@app.route("/<red>/<green>/<blue>/<flash>")
-def action(red, green, blue, flash):
+@app.route("/<red>/<green>/<blue>/<flash>/<musicIndex>/<isSent>")
+def action(red, green, blue, flash, musicIndex, isSent):
 	#   Send same url with browserAction changed to "no" to other bone.
 	os.system("pkill -9 -f ./test.py")
-	os.system("./test.py "+red+" "+green+" "+blue+" "+flash+" &")
+	os.system("./test2.py "+red+" "+green+" "+blue+" "+flash+" &")
+	os.system("pkill -f mplayer")
+	if(isSent == "false"):
+		os.system("wget --spider --server-response vandensp.pagekite.me:8081/"+red+"/"+green+"/"+blue+"/"+musicIndex+"/"+"true")
+		os.system("mplayer -ao alsa:device=sysdefault=Device ../mp3folder/"+files[int(musicIndex)])
+	if(isSent == "true"):
+		os.system("mplayer -ao alsa:device=sysdefault=Device ../mp3folder/"+files[int(musicIndex)])
+	
 	templateData={
 	}
 	return render_template('remoteVirtual.html', **templateData)
